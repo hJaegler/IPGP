e766c2c7dc5bd25adff7184a6ed049774befab4c
Hugo JAEGLER <hugo.jaegler@gmail.com>
2013-07-04 14:27:27 +0200

    script v4
    grand ménage dans le script : 
    * 'class' et 'def' avant le 'main',
    * creation de variables globales dans le 'main'

--- script_v4.py
+++ script_v4.py
@@ -1,5 +1,5 @@
 #! user/hugojaegler/Obspy/bin/python
-# _*_coding:Utf-8 _* 
+# -*- coding: Utf-8 -*-
 '''
 Created on 2 juil. 2013
 
@@ -8,39 +8,33 @@ Created on 2 juil. 2013
 
 import numpy as np
 import matplotlib.pyplot as plt
-from pylab import *
+from pylab  import vlines
 from obspy.core import read
 from Tkinter import *
 
-
-cbuts = []
-cbuts_text = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm']
-cbuts_var = []
-
-
-for index in enumerate(cbuts_text) : 
-    cbuts_var.append(0)
-
+        
 class Interface(Frame):
     '''
-    Interface graphique permettant de sélectionner les paramètres.
+    Interface graphique permettant de selectionner les parametres.
 
     '''
-    def __init__(self, fenetre, **kwargs):
-        Frame.__init__(self, fenetre, ** kwargs)
+    def __init__(self, master=None):
+        Frame.__init__(self, master)
         self.pack(fill=BOTH)
         
         self.var = []
         for index in enumerate(cbuts_text) : 
-                self.var.append(0)
+            self.var.append(0)
                 
         self.message = Label(self, text="click on parameters")
         self.message.pack()
         
         self.create_cbuts()
         
-        Button(self, text = 'select all', command = self.select_all).pack()
-        Button(self, text = 'select none', command = self.deselect_all).pack()
+        self.Button_select = Button(self, text = 'select all', command = self.select_all)
+        self.Button_select.pack()
+        self.Button_deselect = Button(self, text = 'select none', command = self.deselect_all)
+        self.Button_deselect.pack() 
         
         self.bouton_enregistrer = Button(self, text="Record and Quit", command=self.cb)
         self.bouton_enregistrer.pack()
@@ -63,70 +57,39 @@ class Interface(Frame):
     def deselect_all(self):
         for i in cbuts : 
             i.deselect()
-        
-        
-fenetre = Tk()
-interface = Interface(fenetre)
-
-      
-interface.mainloop()
-interface.destroy()
-print cbuts_var
-
-
-'''
-On va maintenant demander d'entrer le nom du sismo. On affichera les paramètres de ce sismo 
-ainsi que la courbe où l'on pourra pointer.
-'''
-
-X = read(raw_input('Which file\'s path ? ')) 
-
-tr = X[0]
 
 class RecapEtOptionsCalculs(Frame):
     '''
-    #Interface graphique récapitulant les caractéristique du sismogramme
-    #présentant les options de filtrage et de calculs du noyau de sensibilité 
+    Interface graphique recapitulant les caracteristique du sismogramme
+    presentant les options de filtrage et de calculs du noyau de sensibilite
     '''
     def __init__(self, master=None):
         Frame.__init__(self, master)
         self.pack(fill=BOTH)
-        self.message = Label(self, text="Récapitulatif du sismogramme")
+        self.message = Label(self, text="Recapitulatif du sismogramme")
         self.message.pack()
         
         self.recap = Text(self, height =12, width = 70)
-        self.recap.insert(INSERT, "network: {}\n".format(tr.stats.network))
-        self.recap.insert(INSERT, "station: {}\n".format(tr.stats.station))
-        self.recap.insert(INSERT, "location: {}\n".format(tr.stats.location))
-        self.recap.insert(INSERT, "channel: {}\n".format(tr.stats.channel))
-        self.recap.insert(INSERT, "starttime: {}\n".format(tr.stats.starttime))
-        self.recap.insert(INSERT, "endtime: {}\n".format(tr.stats.endtime))
-        self.recap.insert(INSERT, "sampling rate: {}\n".format(tr.stats.sampling_rate))
-        self.recap.insert(INSERT, "delta: {}\n".format(tr.stats.delta))
-        self.recap.insert(INSERT, "number points: {}\n".format(tr.stats.npts))
-        self.recap.insert(INSERT, "calibration: {}\n".format(tr.stats.calib))
-        self.recap.insert(INSERT, "format: {}\n".format(tr.stats._format))
+        self.recap.insert(INSERT, "network: {}\n".format(X[0].stats.network))
+        self.recap.insert(INSERT, "station: {}\n".format(X[0].stats.station))
+        self.recap.insert(INSERT, "location: {}\n".format(X[0].stats.location))
+        self.recap.insert(INSERT, "channel: {}\n".format(X[0].stats.channel))
+        self.recap.insert(INSERT, "starttime: {}\n".format(X[0].stats.starttime))
+        self.recap.insert(INSERT, "endtime: {}\n".format(X[0].stats.endtime))
+        self.recap.insert(INSERT, "sampling rate: {}\n".format(X[0].stats.sampling_rate))
+        self.recap.insert(INSERT, "delta: {}\n".format(X[0].stats.delta))
+        self.recap.insert(INSERT, "number points: {}\n".format(X[0].stats.npts))
+        self.recap.insert(INSERT, "calibration: {}\n".format(X[0].stats.calib))
+        self.recap.insert(INSERT, "format: {}\n".format(X[0].stats._format))
         self.recap.pack()
         self.bouton_quitter = Button(self, text="continue", command=self.quit)
-        self.bouton_quitter.pack(side="bottom")
-        
-fenetre = RecapEtOptionsCalculs()
-fenetre.master.title("Recap et Options de Calculs")
-fenetre.master.maxsize(300, 500)
-fenetre.mainloop()
+        self.bouton_quitter.pack(side="bottom")    
 
-
-fig = plt.figure()
-ax = fig.add_subplot(111)
-ax.set_title('click on graphic to store data')
-line, = ax.plot(np.arange(0, len(X[0])) , X[0].data, 'k')
-plt.xlim(0, len(X[0]))
-ymin, ymax = plt.ylim(X[0].data.min()*1.1, X[0].data.max()*1.1)
-
-enregx=[]
-enregy=[]
-
-class LineBuilder : 
+class LineBuilder :
+    '''
+    Fonction permettant de creer des lignes verticales sur un sismo et
+    d'enregistrer les coordonees des endroits pointes sur la courbe
+    ''' 
     def __init__(self, line):
         self.line = line
         self.xs = list(line.get_xdata())
@@ -147,16 +110,74 @@ def onclick(event):
     enregx.append(event.xdata)
     enregy.append(X[0].data[event.xdata])
     vlines(event.xdata, ymin, ymax, color='k', linestyles='dashed')
+        
+ 
+ 
+ 
+def main():
     
-fig.canvas.mpl_connect('button_press_event', onclick)
+    '''
+    Premiere partie : interface graphique permettant de choisir les parametres
+    '''
+    global cbuts
+    cbuts = []
+    global cbuts_text 
+    cbuts_text = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm']
+    global cbuts_var 
+    cbuts_var= []
 
 
-line, = ax.plot([0], [0])
-linebuilder = LineBuilder(line)
-plt.show()
+    for index in enumerate(cbuts_text) : 
+        cbuts_var.append(0)
+        
+    interface = Interface()
 
-print enregx    # Abscisse des points sélectionnés 
-print enregy    # Ordonnées lues sur le sismo
+    interface.mainloop()
+    interface.destroy()
+    print cbuts_var
+    
+    '''
+    Deuxieme partie : l'utilisateur entre le chemin du sismo. On affiche
+    alors les caracteristiques du sismo
+    '''
+    global X
+    X = read(raw_input('Which file\'s path ? ')) 
+    
+    fenetre = RecapEtOptionsCalculs()
+    fenetre.master.title("Recap et Options de Calculs")
+    fenetre.master.maxsize(300, 500)
+    fenetre.mainloop()
+   
+    '''
+    Troisieme partie : on affiche le sismo. L'utilisateur peut pointer
+    sur le sismo et on enregistrer ces coordonnees sur la courbe.
+    '''
+
+    fig = plt.figure()
+    ax = fig.add_subplot(111)
+    ax.set_title('click on graphic to store data')
+    line, = ax.plot(np.arange(0, len(X[0])) , X[0].data, 'k')
+    plt.xlim(0, len(X[0]))
+    global ymin
+    global ymax
+    ymin, ymax = plt.ylim(X[0].data.min()*1.1, X[0].data.max()*1.1)
+    
+    global enregx
+    global enregy
+    enregx=[]
+    enregy=[]
+
+   
+    fig.canvas.mpl_connect('button_press_event', onclick)
+
+
+    line, = ax.plot([0], [0])
+    linebuilder = LineBuilder(line)
+    plt.show()
+
+    print enregx    # Abscisse des points selectionnes 
+    print enregy    # Ordonnees lues sur le sismo
+    
 
 if __name__ == '__main__':
-    pass
+    main()
